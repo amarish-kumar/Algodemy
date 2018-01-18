c378fccd168a99959938149b20090cebdc505b05
Elvis <dreemsuncho@gmail.com>
2018-01-18 18:12:17 +0200

    First project push

---
+++ Algodemy/Pages/Index.cshtml.cs
@@ -0,0 +1,9 @@
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+        
+        namespace Algodemy.Pages
+        {
+            public class IndexModel : PageModel
+            {
+                public void OnGet() { }
+            }
+        }
